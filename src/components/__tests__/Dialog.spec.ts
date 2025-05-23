ff54c5d431388fb6449e6a8e6ae88ad31ffd0750
Dmitriy Rudnik <dmitriyrudnik@gmail.com>
2025-05-23 19:14:04 +0300

    add spec for Dialog component

---
+++ src/components/__tests__/Dialog.spec.ts
@@ -0,0 +1,42 @@
+import {describe, expect, it} from 'vitest'
+import {mount} from '@vue/test-utils'
+import {nextTick} from 'vue'
+import Dialog from '../Dialog.vue'
+
+describe('Dialog', () => {
+  it('should open dialog and handle cancel', async () => {
+    const wrapper = mount(Dialog)
+    const vm = wrapper.vm as InstanceType<typeof Dialog>
+    const openPromise = vm.open()
+    await nextTick()
+    
+    expect(wrapper.find('.el-dialog').exists()).toBe(true)
+    
+    const buttons = wrapper.findAll('button')
+    const cancelButton = buttons.find(button => button.text() === 'Cancel')
+    expect(cancelButton).toBeDefined()
+    
+    await cancelButton!.trigger('click')
+    await expect(openPromise).rejects.toBeUndefined()
+  })
+  
+  it('should open dialog and handle confirm', async () => {
+    const wrapper = mount(Dialog)
+    const vm = wrapper.vm as InstanceType<typeof Dialog>
+    const openPromise = vm.open()
+    await nextTick()
+    const dialog = wrapper.find('.el-dialog')
+    
+    expect(dialog.exists()).toBe(true)
+    expect(dialog.isVisible()).toBe(true)
+    
+    const buttons = wrapper.findAll('button')
+    const confirmButton = buttons.find(button => button.text() === 'Confirm')
+    expect(confirmButton).toBeDefined()
+    
+    await confirmButton!.trigger('click')
+    await expect(openPromise).resolves.toBeUndefined()
+    
+    expect(dialog.isVisible()).toBe(false)
+  })
+})
