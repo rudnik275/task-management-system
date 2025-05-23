13615293d2f432c4d7827b8ba1911646110d19c9
Dmitriy Rudnik <dmitriyrudnik@gmail.com>
2025-05-23 18:46:50 +0300

    update vitest config to fix error with css import

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import {fileURLToPath} from 'node:url'
+import {mergeConfig, defineConfig, configDefaults} from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -9,6 +9,11 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      server: {
+        deps: {
+          inline: ['element-plus']
+        }
+      }
     },
   }),
 )
