d507d06ff241d3c74f9db2d955f4462a97bf848f
Dmitriy Rudnik <dmitriyrudnik@gmail.com>
2025-05-29 11:48:32 +0300

    redesign auth guard demo section

--- src/App.vue
+++ src/App.vue
@@ -15,30 +15,25 @@ onMounted(() => {
 </script>
 
 <template>
+  <ElAlert
+    title="Demo section of auth token guard"
+    type="info"
+    description=""
+    show-icon
+  >
+    <ElSpace direction="vertical" alignment="flex-start">
+      <span>Removing of token should returns all mocked routes except /proects with 401 error</span>
+      <ElButton @click="hasAuthToken = !hasAuthToken">
+        {{ hasAuthToken ? 'Delete auth token' : 'Rollback auth token' }}
+      </ElButton>
+    </ElSpace>
+  </ElAlert>
   <ElContainer class="container">
     <ElMain>
       <RouterView :key="route.path"/>
-
-      <section class="auth-token-demo">
-        <h2>Demo section of auth token guard</h2>
-        <ElText type="info">Removing of token should returns all mocked routes except /proects with 401 error</ElText>
-        <ElButton @click="hasAuthToken = !hasAuthToken">
-          {{ hasAuthToken ? 'Delete auth token' : 'Rollback auth token' }}
-        </ElButton>
-      </section>
     </ElMain>
   </ElContainer>
 </template>
 
 <style>
-.auth-token-demo {
-  margin-top: 40px;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  font-size: 20px;
-  background: #e5e5e5;
-}
 </style>
